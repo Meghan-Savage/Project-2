68d0d60ddeb88a6cccaed4f27c0efe96d53eaad5
Meghan-Savage <MSavage1@protonmail.com>
2023-04-16 20:07:23 -0600

    created basic landing page

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,7 +1,28 @@
 import React from "react";
 
 const LandingPage = () => {
-  return <div>LandingPage</div>;
+  return (
+    <div className="flex">
+      <div className="m-10 w-full">
+        <img
+          src="/Grocery-Items-With-the-Biggest-Pandemic-Price-Increase.jpg"
+          alt="Grocery bag with items"
+        />
+      </div>
+      <div className="w-full m-10 ">
+        <h1 className="font-bold text-6xl">CHECK IT OUT</h1>
+        <p className="m-10 text-xl">
+          Simplify your grocery shopping and save money with our customizable
+          favorites and weekly buy lists! Say goodbye to wandering aimlessly
+          through the store and hello to a smarter, more efficient shopping
+          experience. Create your lists, check them twice, and enjoy the
+          satisfaction of crossing everything off with ease. Shop smarter, not
+          harder, with our user-friendly platform. Join us today and make your
+          grocery shopping a breeze!
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default LandingPage;
