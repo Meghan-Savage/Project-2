662b096a242a5ed8c299bb30bd060e487f81b2df
Renata Vieira <rrenattavs@gmail.com>
2023-04-18 23:46:55 -0600

    added the file user.js in the controller folder

---
+++ server/controller/user.js
@@ -0,0 +1,46 @@
+import User from "../../src/model/User.js"
+
+//create new user is controllers/auth.js
+//login is in auth.js
+
+export const updateUser = async(req, res, next) =>{
+    try{
+        const updatedUser = await User.findByIdAndUpdate(
+            req.params.id,
+            {$set: req.body },
+            {new: true}
+        );
+        res.status(200).json(updatedUser);
+    }catch (err){
+        next(err);
+    }
+}
+
+export const deleteUser = async(req, res, next) =>{
+    try{
+        const deletedUser = await User.findByIdAndDelete(
+            req.params.id,
+        );
+        res.status(200).json(deletedUser);
+    }catch (err){
+        next(err);
+    }
+}
+
+export const getUser = async(req, res, next) =>{
+    try{
+        const user = await User.findById(req.params.id);
+        res.status(200).json(users);
+    }catch (err){
+        next(err);
+    }
+}
+
+export const getUsers = async(req, res, next) =>{
+    try{
+        const users = await User.find();
+        res.status(200).json(users);
+    }catch (err){
+        next(err);
+    }
+}
