56c27c99b2508e42e56f7a2fd7112ccf8837e015
Renata Vieira <rrenattavs@gmail.com>
2023-04-27 00:11:11 -0600

    remove the function register and changePassword

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,14 +1,42 @@
+import {login, logout} from "../controller/auth.js"
 import express from "express";
-import {register, login} from "../controller/auth.js"
+import passport from "passport";
+import LocalStrategy from "passport-local";
+import { verifyPassword } from "../../src/model/User.js";
+const router = express.Router();
+passport.use(
+  new LocalStrategy(async function (username, password, done) {
+    const user = await verifyPassword(username, password);
+    if (!user) {
+      return done(null, false);
+    } else {
+      return done(null, user);
+    }
+  })
+);
 
-const router = express.Router(); 
+passport.serializeUser(function (user, cb) {
+  process.nextTick(function () {
+    return cb(null, user);
+  });
+});
+
+passport.deserializeUser(function (user, cb) {
+  process.nextTick(function () {
+    return cb(null, user);
+  });
+});
+
+router.post("/login", passport.authenticate("local"), login);
+
+router.post("/logout", logout);
 
 router.get("/", (req, res) => {
-    res.send("Hello , this is auth endpoint");
-})
-
-router.post("/register", register)
-router.post("/login", login)
-
+  //artificially slowing down this request to see loading screen
+  setTimeout(() => {
+    if (req.user) res.send(req.user);
+    else res.sendStatus(401);
+  }, 2000);
+});
 
 export default router;
