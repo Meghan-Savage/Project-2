b9a98133bf961be86f2abb77e757f7631206c658
Renata Vieira <rrenattavs@gmail.com>
2023-04-27 00:08:15 -0600

    insert two functions (createUser, verifyPassword)

--- src/model/User.js
+++ src/model/User.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt"
 
 const userSchema = mongoose.Schema(
   {
@@ -7,10 +8,34 @@ const userSchema = mongoose.Schema(
     password: { type: String, required: true },
     name: { type: String },
     isAdmin: { type: Boolean, default: false },
-  },
-  { timestamps: true }
-);
+  });
 
-const User = mongoose.model("user", userSchema);
+const User = mongoose.model("user", userSchema, "users");
+
+export const createUser = async (newUser) => {
+  try {
+    const hashedPassword = bcrypt.hashSync(newUser.password, 10);
+    const createdUser = await User.create({
+      ...newUser,
+      password: hashedPassword,
+    });
+    return createdUser;
+  } catch (error) {
+    if (error.code === 11000) {
+      throw new Error("Duplicate Error");
+    }
+  }
+};
+
+export const verifyPassword = async (email, password) => {
+  const user = await User.findOne({ email });
+  if (user && bcrypt.compareSync(password, user.password)) {
+    const userData = user.toObject();
+    delete userData.password;
+    return userData;
+  } else {
+    return null;
+  }
+};
 
 export default User;
