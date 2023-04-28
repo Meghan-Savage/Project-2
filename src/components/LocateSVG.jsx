874d492e82bc2a4ce91dfb0877e4f53e03b251e9
Larry Hengen <lhengen@tpersistent.com>
2023-04-28 01:27:52 -0600

    code cleanup

--- src/components/LocateSVG.jsx
+++ src/components/LocateSVG.jsx
@@ -5,7 +5,7 @@ function LocateSVG() {
   const apiURL = "https://ipgeolocation.abstractapi.com/v1/";
   const apiKey = "df635717a4374cf28b49705a8fc81e86";
 
-  const [zipCode, setZipCode] = useState("");
+  const [postalCode, setPostalCode] = useState("");
   const [LatLong, SetLatLong] = useState(CalgaryLatLong);
 
   const getUserLocationFromIP = async () => {
@@ -13,18 +13,12 @@ function LocateSVG() {
     try {
       console.log("getting location from IP " + fullURL);
       const response = await (await fetch(fullURL)).json(); //get data and convert JSON to an Object
-
-      console.log("response", response);
-      console.log("response.latitude", response.latitude);
-      console.log("response.longitude", response.longitude);
       const NewLatLong = {
         Latitude: response.latitude,
         Longitude: response.longitude,
       };
-      console.log("NewLatLong", NewLatLong);
       SetLatLong(NewLatLong);
-      console.log("PostalCode", response.postal_code);
-      setZipCode(response.postal_code);
+      setPostalCode(response.postal_code);
     } catch (error) {
       console.error(error);
     }
@@ -33,29 +27,19 @@ function LocateSVG() {
   return (
     <div>
       <div>
-        <p>{zipCode}</p>
         <br />
-        <label>Postal Code</label>
-
-        <input
-          name="PostalCode"
-          type="text"
-          color="rebeccapurple"
-          size="10"
-        ></input>
         <button
           type="button"
+          // value="IP"
+          viewBox="0 0 48 48"
           onClick={() => {
             getUserLocationFromIP();
           }}
         >
-          Press Me
+          {"IP"}
         </button>
 
         <br />
-        <label>
-          Lat: {LatLong.Latitude} Long: {LatLong.Longitude}
-        </label>
         <br />
         <button
           onClick={() => {
@@ -70,7 +54,7 @@ function LocateSVG() {
           }}
           color="rebeccapurple"
         >
-          Use Current Location
+          {"Nav"}
           <svg viewBox="0 0 48 48" className="css-v6j1qa eu14tha6">
             <path d="M46 22h-4.12A18 18 0 0 0 26 6.12V2a2 2 0 0 0-4 0v4.12A18 18 0 0 0 6.12 22H2a2 2 0 0 0 0 4h4.12A18 18 0 0 0 22 41.88V46a2 2 0 0 0 4 0v-4.12A18 18 0 0 0 41.88 26H46a2 2 0 0 0 0-4zM24 38a14 14 0 1 1 14-14 14 14 0 0 1-14 14z"></path>
             <circle cx="24" cy="24" r="4"></circle>
@@ -82,31 +66,3 @@ function LocateSVG() {
 }
 
 export default LocateSVG;
-
-// const apiURL = 'https://ipgeolocation.abstractapi.com/v1/'
-// const apiKey = 'df635717a4374cf28b49705a8fc81e86';
-
-// function App() {
-//  const [zipCode, setZipCode] = useState("");
-//  const apiURL = 'https://ipgeolocation.abstractapi.com/v1/'
-//  const apiKey = 'df635717a4374cf28b49705a8fc81e86';
-
-//  const getUserLocation = async () => {
-// 	const fullURL = apiURL + "?api_key=" + apiKey;
-// 		try {
-// 			const response = await fetch(fullURL);
-// 			const zip = response.postal_code;
-// 			setZipCode(zip);
-// 		} catch (error) {
-// 			console.error(error)
-// 		}
-//  }
-
-//  function componentDidMount() {
-// 	getUserLocation()
-//  }
-
-//  return (
-//   <div className="App">
-//  <p>{zipCode}</p>
-//   </div>
