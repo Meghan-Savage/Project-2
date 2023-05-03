bcdaa8280150c22d4317b287341a9f0f9f56569c
Renata Vieira <rrenattavs@gmail.com>
2023-05-02 22:59:58 -0600

    Return the original file

--- src/components/LoginInForm.jsx
+++ src/components/LoginInForm.jsx
@@ -5,32 +5,12 @@ import BlockInput from "../util/BlockInput";
 
 
 const LoginInForm = () =>{
-  // const [username, setUsername] = useState('');
-  // const[password, setPassword] = useState('');
-  // const[error, setError] = useState(null)
-  // const navigate = useNavigate();
-  // const { login } = useAuth();
-
-  // const handleLogin = async (e) => {
-  //   e.preventDefault();
-  //   const isValid = await login(username, password);
-  //   if(!isValid){
-  //     setError("Incorrect username or password")
-  //   }else {
-  //     navigate(-1)
-  //   }
-  // }
 
   return (
-    // <form action="" method="POST" onSubmit={handleLogin}>
-    <form action="" method="POST">
+    <form action="http://localhost:3000/login" method="POST">
       <div>
-        <BlockInput type="text" placeholder="USERNAME" name="username"
-        // value={username} onChange={event => setUsername(event.target.value)}
-        />
-        <BlockInput type="password" placeholder="PASSWORD" name="password" 
-        // value={password} onChange={event => setPassword(event.target.value)}
-        />
+        <BlockInput type="text" placeholder="USERNAME" name="username"/>
+        <BlockInput type="password" placeholder="PASSWORD" name="password" />
       </div>
       <div>
         <button>forgot your password?</button>
@@ -41,7 +21,6 @@ const LoginInForm = () =>{
           SIGN IN
         </button>
       </div>
-      {/* {error && <div>{error}</div>} */}
     </form>
   );
 };
