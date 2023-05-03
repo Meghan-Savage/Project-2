4be6ff2ca3a6b918df05fd8ce6490f044c2161e4
Renata Vieira <rrenattavs@gmail.com>
2023-05-02 23:36:33 -0600

    return the original file

--- src/components/LoginInForm.jsx
+++ src/components/LoginInForm.jsx
@@ -1,36 +1,15 @@
-import React, {useState} from "react";
+import React from "react";
 import BlockInput from "../util/BlockInput";
-// import { useNavigate } from "react-router-dom";
 // import {useAuth} from "../providers/AuthProvider"
 
 
 const LoginInForm = () =>{
-  // const [username, setUsername] = useState('');
-  // const[password, setPassword] = useState('');
-  // const[error, setError] = useState(null)
-  // const navigate = useNavigate();
-  // const { login } = useAuth();
-
-  // const handleLogin = async (e) => {
-  //   e.preventDefault();
-  //   const isValid = await login(username, password);
-  //   if(!isValid){
-  //     setError("Incorrect username or password")
-  //   }else {
-  //     navigate(-1)
-  //   }
-  // }
 
   return (
-    // <form action="" method="POST" onSubmit={handleLogin}>
-    <form action="" method="POST">
+    <form action="http://localhost:3000/login" method="POST">
       <div>
-        <BlockInput type="text" placeholder="USERNAME" name="username"
-        // value={username} onChange={event => setUsername(event.target.value)}
-        />
-        <BlockInput type="password" placeholder="PASSWORD" name="password" 
-        // value={password} onChange={event => setPassword(event.target.value)}
-        />
+        <BlockInput type="text" placeholder="USERNAME" name="username" />
+        <BlockInput type="password" placeholder="PASSWORD" name="password" />
       </div>
       <div>
         <button>forgot your password?</button>
@@ -41,7 +20,6 @@ const LoginInForm = () =>{
           SIGN IN
         </button>
       </div>
-      {/* {error && <div>{error}</div>} */}
     </form>
   );
 };
