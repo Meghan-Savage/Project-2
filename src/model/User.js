bce395786969d17c359e9a0f681feb5d1e9d9a43
Renata Vieira <rrenattavs@gmail.com>
2023-05-02 18:45:41 -0600

    added useNavigate in the logininform.jsx

--- src/model/User.js
+++ src/model/User.js
@@ -13,34 +13,4 @@ const userSchema = mongoose.Schema(
 
 const User = mongoose.model("user", userSchema);
 
-
-
-// const User = mongoose.model("user", userSchema, "users");
-
-// export const createUser = async (newUser) => {
-//   try {
-//     const hashedPassword = bcrypt.hashSync(newUser.password, 10);
-//     const createdUser = await User.create({
-//       ...newUser,
-//       password: hashedPassword,
-//     });
-//     return createdUser;
-//   } catch (error) {
-//     if (error.code === 11000) {
-//       throw new Error("Duplicate Error");
-//     }
-//   }
-// };
-
-// export const verifyPassword = async (email, password) => {
-//   const user = await User.findOne({ email });
-//   if (user && bcrypt.compareSync(password, user.password)) {
-//     const userData = user.toObject();
-//     delete userData.password;
-//     return userData;
-//   } else {
-//     return null;
-//   }
-// };
-
 export default User;
